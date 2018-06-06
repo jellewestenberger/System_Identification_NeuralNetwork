5c67d2938fda79c87721df4516f0159d4c017d3a
Unknown <jellewestenberger@gmail.com>
2018-06-06 22:33:31 +0200

    Added least-squares estimation

--- RBFNN.m
+++ RBFNN.m
@@ -12,26 +12,54 @@ Networktype='rbf'; %choose network type: radial basis function (rbf) or feedforw
 nrInput=size(X,2);     %number of inputs being used
 
 nrOutput=1; 
-nrNodesHidden=[3]; %add columns to add more hidden layers;
+nrNodesHidden=[100]; %add columns to add more hidden layers;
 nrHiddenlayers=size(nrNodesHidden,2);%number of nodes in the hidden layers
 inputrange=[min(X); max(X)]';
 
 
 NNset=createNNStructure(nrInput,nrHiddenlayers,nrNodesHidden,nrOutput,inputrange,Networktype);
   
-%---CHECK----
+%% ---CHECK---- %% 
  check=NNCheck(NNset,nrInput,nrNodesHidden,nrOutput);
-%------------
+%% ------------ %% 
 
-
+X=X';
 
 % Nin=size(X
-%Creating output---
-X=X';
+
+
+%% Linear regression 
+%not that this is currently only valid for a 1 hidden layer network
+Phis=zeros(size(X,2),nrNodesHidden);
+
+A=zeros(size(X,2),nrNodesHidden);
+a=zeros(nrNodesHidden,1);
+
+for j=1:nrNodesHidden %node identifier 
+
+eval=0;
+for i=1:size(X,1) %input identifier (alpha, beta or V) 
+x=X(i,:);
+w=NNset.IW{1,1}(j,i); %input weight;
+c=NNset.centers{1,1}(j,i); %centers
+eval=eval+(w^2)*(x-c).^2;     %input variable for activation function (consists of sum of input weights multiplied with corresponding center differences)       
+end
+Phis(:,j)=exp(-eval)';
+end
+A(:,:)=Phis.*NNset.LW;
+a=((A'*A)^(-1))*A'*Cm;
+Cm_estimated=A*a;
+NNset.a=a;
+%%
+%% Creating output---
+
 Y=calcNNOutput(NNset,X);
 
-
+%% plotting 
 figure
-plot3(atrue,Btrue,Y','.');
+plot3(atrue,Btrue,Y','.b'); 
+hold on
+plot3(atrue,Btrue,Cm_estimated,'.');
 hold on
 plot3(atrue,Btrue,Cm,'.k');
+%%
--- calcNNOutput.m
+++ calcNNOutput.m
@@ -1,20 +1,21 @@
 function Y2=calcNNOutput(NNset,X)
-for h=1:size(NNset.centers,2) %loop over number of hidden layers
-Nin=size(X,1);
-L_end=size(X,2);
-Nhidden=size(NNset.centers{h},1);
-V1 = zeros(Nhidden,L_end);
 
-for i=1:Nin
-xij=X(i,:).*ones(size(V1));
-% disp(NNset.centers(:,i));
-cij=NNset.centers{h}(:,i)*ones(1,L_end);
-wj=NNset.IW{h}(:,i);
-V1=V1+(wj.*(xij-cij)).^2;
-end;
-%output for hidden layer
-Y1=-1*exp(-V1);
-X=Y1; %output of current hidden layer is input for next hidden layer
+for h=1:size(NNset.centers,2) %loop over number of hidden layers
+    Nin=size(X,1);
+    L_end=size(X,2);
+    Nhidden=size(NNset.centers{h},1);
+    V1 = zeros(Nhidden,L_end);
+
+    for i=1:Nin
+        xij=X(i,:).*ones(size(V1));
+        % disp(NNset.centers(:,i));
+        cij=NNset.centers{h}(:,i)*ones(1,L_end);
+        wj=NNset.IW{h}(:,i);
+        V1=V1+(wj.*(xij-cij)).^2;
+    end;
+    %output for hidden layer
+    Y1=NNset.a(:,h).*exp(-V1);
+    X=Y1; %output of current hidden layer is input for next hidden layer
 end
 
 
