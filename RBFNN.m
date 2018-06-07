c41ab3eb3eaff2fe2a73f208abcd2dc952437c02
Unknown <jellewestenberger@gmail.com>
2018-06-07 22:56:15 +0200

    Finished error/weight derivatives

--- RBFNN.m
+++ RBFNN.m
@@ -3,7 +3,7 @@ close all
 load('atrue.mat');
 load('Btrue.mat');
 load('Vtrue.mat');
-load('NetExampleRBF.mat');
+% load('NetExampleRBF.mat');
 load('T.mat');
 load('F16traindata_CMabV_2018','Cm');
 X=[atrue, Btrue, Vtrue]; %input vector 
@@ -12,7 +12,7 @@ Networktype='rbf'; %choose network type: radial basis function (rbf) or feedforw
 nrInput=size(X,2);     %number of inputs being used
 
 nrOutput=1; 
-nrNodesHidden=[100]; %add columns to add more hidden layers;
+nrNodesHidden=[6]; %add columns to add more hidden layers;
 nrHiddenlayers=size(nrNodesHidden,2);%number of nodes in the hidden layers
 inputrange=[min(X); max(X)]';
 
@@ -48,21 +48,33 @@ end
 A(:,:)=Phis.*NNset.LW;
 a=((A'*A)^(-1))*A'*Cm;
 Cm_estimated=A*a;
-NNset.a=a;
+NNset.a{1}=a;
 %%
 %% Creating output---
 
-[Y, hiddenoutput]=calcNNOutput(NNset,X); %nice for double checking with Cm_estimated (should be exactly equal)
+[Y, hiddenoutput, Vj]=calcNNOutput(NNset,X); %nice for double checking with Cm_estimated (should be exactly equal)
 
 %% Levenberg Marquard
 %calculate dE/d Wjk   (Wjk = output weight);
-ek=Y'-Cm;
-h=1
-dEdWjk{h}=zeros(size(NNset.IW{h},1),1);
-
-for j=1:size(Wjk{h},1)
+ekq=Y'-Cm;
+k=1;
+dEdWjk{k}=zeros(size(NNset.IW{k},1),1); %output weights
+dEdWij{k}=zeros(size(NNset.IW{k}));  %input weights 
+dPhidVj{k}=zeros(size(NNset.IW{k},1),1);  %partial derivatives dphij/dvj
+for j=1:size(dEdWjk{k},1)
 yj=hiddenoutput{1}(j,:);    
-dEdWjk{h}(j)=sum(ek.*yj');
+dEdWjk{k}(j)=sum(-1*ekq.*yj');
+dPhidVj{k}=-NNset.a{k}.*exp(-Vj{k});
+end
+for i=1:size(NNset.IW{k},2) 
+   s=X(i,:).*dPhidVj{k};
+   s=-ekq'.*s;
+   dEdWij{k}(:,i)=sum(s,2);
+end
+
+mu=1;
+for k = 1:size(NNset.IW,1)
+    IW=-((dEdWij{k}'*dEdWij{k}+mu*eye(size(dEdWij{k},2)))^(-1))*dEdWij{k}'
 end
 
 
--- calcNNOutput.m
+++ calcNNOutput.m
@@ -1,23 +1,25 @@
-function [Y2, hiddenoutput]=calcNNOutput(NNset,X)
+function [Y2, hiddenoutput,Vj]=calcNNOutput(NNset,X)
 hiddenoutput={};
-for h=1:size(NNset.centers,2) %loop over number of hidden layers
-    hiddenoutput{h}=zeros(size(NNset.IW{h},1),size(X,2));
+Vj={};
+for k=1:size(NNset.centers,2) %loop over number of hidden layers
+    hiddenoutput{k}=zeros(size(NNset.IW{k},1),size(X,2));
+    
     Nin=size(X,1);
     L_end=size(X,2);
-    Nhidden=size(NNset.centers{h},1);
+    Nhidden=size(NNset.centers{k},1);
     V1 = zeros(Nhidden,L_end);
 
     for i=1:Nin
         xij=X(i,:).*ones(size(V1));
         % disp(NNset.centers(:,i));
-        cij=NNset.centers{h}(:,i)*ones(1,L_end);
-        wj=NNset.IW{h}(:,i);
+        cij=NNset.centers{k}(:,i)*ones(1,L_end);
+        wj=NNset.IW{k}(:,i);
         V1=V1+(wj.*(xij-cij)).^2;
     end;
     %output for hidden layer
-    Y1=NNset.a(:,h).*exp(-V1);
-    
-    hiddenoutput{h}(:,:)=Y1;
+    Y1=NNset.a{k}(:,k).*exp(-V1);
+    Vj{k}=V1;
+    hiddenoutput{k}(:,:)=Y1;
     X=Y1; %output of current hidden layer is input for next hidden layer
 end
 
