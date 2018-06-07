f23982bb84af24528344840ec96142c51b82914f
Jelle Westenberger <jellewestenberger@gmail.com>
2018-06-07 17:39:43 +0200

    Started LM algorithm

--- ParEstimator.m
+++ ParEstimator.m
@@ -12,19 +12,28 @@ load('T.mat');
 
 
 vars=3; %number of variables (in this case alpha, beta and 
-n=3; %polynomial order
-A=zeros(size(T,2),vars*n+1);
+n=6; %polynomial order
+% A=zeros(size(T,2),vars*n+1);
+A=zeros(size(T,2),n+1);
 A(:,1)=1;
 
-for i=1:n
-   if vars==3
-   blck=[atrue.^i Btrue.^i Vtrue.^i];
-   end
-   if vars==2
-       blck=[atrue.^i Btrue.^i];
-   end
-   A(:,(2+(i-1)*vars):((2+(i-1)*vars)+vars-1))=blck;
+for i=1:n 
+    A(:,i+1)=(atrue(:)+Btrue(:)).^i;
+    if i>1
+        A(:,i+1)=A(:,i+1)+A(:,i);
+    end
 end
+
+
+% for i=1:n
+%    if vars==3
+%    blck=[atrue.^i Btrue.^i Vtrue.^i];
+%    end
+%    if vars==2
+%        blck=[atrue.^i Btrue.^i];
+%    end
+%    A(:,(2+(i-1)*vars):((2+(i-1)*vars)+vars-1))=blck;
+% end
 
 est=((A'*A)^(-1))*A'*Cm;
 estimatedCm=A*est;
