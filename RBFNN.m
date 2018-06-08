a6ad9ffa273c4116e18de006223a3e530bbaa1ef
Jelle Westenberger <jellewestenberger@gmail.com>
2018-06-08 10:34:04 +0200

    update

--- RBFNN.m
+++ RBFNN.m
@@ -12,7 +12,7 @@ Networktype='rbf'; %choose network type: radial basis function (rbf) or feedforw
 nrInput=size(X,2);     %number of inputs being used
 
 nrOutput=1; 
-nrNodesHidden=[6]; %add columns to add more hidden layers;
+nrNodesHidden=[10]; %add columns to add more hidden layers;
 nrHiddenlayers=size(nrNodesHidden,2);%number of nodes in the hidden layers
 inputrange=[min(X); max(X)]';
 
@@ -49,40 +49,56 @@ A(:,:)=Phis.*NNset.LW;
 a=((A'*A)^(-1))*A'*Cm;
 Cm_estimated=A*a;
 NNset.a{1}=a;
+sqrter=sum((Cm_estimated-Cm).^2);
 %%
 %% Creating output---
 
-[Y, hiddenoutput, Vj]=calcNNOutput(NNset,X); %nice for double checking with Cm_estimated (should be exactly equal)
 
 %% Levenberg Marquard
 %calculate dE/d Wjk   (Wjk = output weight);
+
+% epoch = 3;
+%  for it=1:epoch
+%     disp(it);
+[Y, hiddenoutput, Vj]=calcNNOutput(NNset,X); %nice for double checking with Cm_estimated (should be exactly equal)
+
 ekq=Y'-Cm;
-k=1;
-dEdWjk{k}=zeros(size(NNset.IW{k},1),1); %output weights
-dEdWij{k}=zeros(size(NNset.IW{k}));  %input weights 
-dPhidVj{k}=zeros(size(NNset.IW{k},1),1);  %partial derivatives dphij/dvj
-for j=1:size(dEdWjk{k},1)
-yj=hiddenoutput{1}(j,:);    
-dEdWjk{k}(j)=sum(-1*ekq.*yj');
-dPhidVj{k}=-NNset.a{k}.*exp(-Vj{k});
-end
-for i=1:size(NNset.IW{k},2) 
-   s=X(i,:).*dPhidVj{k};
-   s=-ekq'.*s;
-   dEdWij{k}(:,i)=sum(s,2);
-end
-
-mu=1;
-for k = 1:size(NNset.IW,1)
-    IW=-((dEdWij{k}'*dEdWij{k}+mu*eye(size(dEdWij{k},2)))^(-1))*dEdWij{k}'
-end
 
 
-%% plotting 
+%% Before starting over
+% disp(sum(ekq));
+% k=1;
+% dEdWjk{k}=zeros(size(NNset.IW{k},1),1); %output weights
+% dEdWij{k}=zeros(size(NNset.IW{k}));  %input weights 
+% dPhidVj{k}=zeros(size(NNset.IW{k},1),1);  %partial derivatives dphij/dvj
+% for j=1:size(dEdWjk{k},1)
+% yj=hiddenoutput{1}(j,:);    
+% dEdWjk{k}(j)=sum(-1*ekq.*yj');
+% dPhidVj{k}=-NNset.a{k}.*exp(-Vj{k});
+% end
+% for i=1:size(NNset.IW{k},2) 
+%    s=X(i,:).*dPhidVj{k}.*NNset.LW';
+%    s=-ekq'.*s;
+%    dEdWij{k}(:,i)=sum(s,2);
+% end
+% 
+% mu=1;
+% for k = 1:size(NNset.IW,1)
+%     IW=NNset.IW{k}'-((dEdWij{k}'*dEdWij{k}+mu*eye(size(dEdWij{k},2)))^(-1))*dEdWij{k}'*sum(ekq);
+%     NNset.IW{k}=IW';
+% end
+% LW=NNset.LW-((dEdWjk{k}'*dEdWjk{k}+mu*eye(size(dEdWjk{k},2)))^(-1))*dEdWjk{k}'*sum(ekq);
+% NNset.LW=LW;
+%  end
+%%
+
+
 figure
 plot3(atrue,Btrue,Y','.b'); 
-hold on
-plot3(atrue,Btrue,Cm_estimated,'.');
+%% plotting 
+
+% hold on
+% plot3(atrue,Btrue,Cm_estimated,'.');
 hold on
 plot3(atrue,Btrue,Cm,'.k');
 %%
